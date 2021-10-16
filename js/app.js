7b90fb855e4ff86f864a3139c7add1aa4465414a
Zahid Srk <69967516+zahid9946@users.noreply.github.com>
2021-10-16 12:23:22 +0600

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,50 @@
+function populate() {
+    if(quiz.isEnded()) {
+        showScores();
+    }
+    else {
+        // show question
+        let element = document.getElementById("question");
+        element.innerHTML = quiz.getQuestionIndex().text;
+
+        // show options
+        let choices = quiz.getQuestionIndex().choices;
+        for(let i = 0; i < choices.length; i++) {
+            let element = document.getElementById("choice" + i);
+            element.innerHTML = choices[i];
+            guess("btn" + i, choices[i]);
+        }
+
+        showProgress();
+    }
+};
+
+function guess(id, guess) {
+    let button = document.getElementById(id);
+    button.onclick = function() {
+        quiz.guess(guess);
+        populate();
+    }
+};
+
+
+function showProgress() {
+    let currentQuestionNumber = quiz.questionIndex + 1;
+    let element = document.getElementById("progress");
+    element.innerHTML = "Question " + currentQuestionNumber + " of " + quiz.questions.length;
+};
+
+function showScores() {
+    let gameOverHTML = "<h1>Result</h1>";
+    gameOverHTML += "<h2 id='score'> Your scores: " + quiz.score + "</h2>";
+    let element = document.getElementById("quiz");
+    element.innerHTML = gameOverHTML;
+};
+
+// create questions: go to the 'question_list.js' file
+
+// create quiz
+let quiz = new Quiz(questions);
+
+// display quiz
+populate();
---
+++ js/question_list.js
@@ -0,0 +1,105 @@
+// Create Demo Questions
+let questions = [
+    new Question(
+        "Assignment Operators is following type of operator ______________.", 
+        ["None of these", "Unary","Binary", "Ternary"], 
+        "Binary"
+    ),
+    new Question(
+        `"Add and Assignment" operator is shown by this symbol.`, 
+        ["==+", "+==", "=+", "+="], 
+        "+="
+    ),
+    new Question(
+        "Which of the following operators does not come under assignment Operator?", 
+        ["=", "==","%=", "+="], 
+        "=="
+    ),
+    new Question(
+        `"===" operator is _______________.`, 
+        ["Is equal to", "Is equal to and is of the same type", "None of these", "Is equal to and is of the different type"], 
+        "Is equal to and is of the same type"
+    ),
+    new Question(
+        `"+=" operator can operate on following data values.`, 
+        ["All Options", "String", "Integer", "Float"], 
+        "All Options"
+    ),
+    new Question(
+        "Which of the following is not an logical operator?", 
+        ["&&", "!","|", "||"], 
+        "!"
+    ),
+    new Question(
+        `Which of the following operator is used to concatenate two strings.`, 
+        ["Comma", "Arrow", "Plus", "Dot"], 
+        "Plus"
+    ),
+    new Question(
+        `What type of value gets printed if we add following two variables. 
+            let a = "1"
+            let b = 5
+        `, 
+        ["Number", "Integer", "Text", "Long"], 
+        "Text"
+    ),
+    new Question(
+        `"++" is __________ type of operator`, 
+        ["Unary", "Ternary", "Binary", "None of these"], 
+        "Unary"
+    ),
+    new Question(
+        `"++" can operate only on ________________.`, 
+        ["Constants", "None of these", "Text", "Variables"], 
+        "Variables"
+    ),
+    new Question(
+        "Integer Variable + Character Variable + Undefined Variable = ______________ Value", 
+        ["Integer", "Undefined Value","Character", "Text / String Value"], 
+        "Undefined Value"
+    ),
+    new Question(
+        `Which of the following is not a comparison operator ?`, 
+        [">=", "<=", "++", "==="], 
+        "++"
+    ),
+    new Question(
+        "Adding String and Integer always results in _________.", 
+        ["None of these", "String","Integer", "Character"], 
+        "String"
+    ),
+    new Question(
+        `JavaScript contains a _________________ that assigns a value to a variable based on some condition.`, 
+        ["Assignment operator", "Logical Operator", "Bitwise Operator", "Conditional operator"], 
+        "Conditional operator"
+    )
+]
+
+// Create Demo Questions
+// let questions = [
+//     new Question(
+//         "Which one is not an object oriented programming language?", 
+//         ["Java", "C#","C++", "C"], 
+//         "C"
+//     ),
+//     new Question(
+//         "Which language is used for styling web pages?", 
+//         ["HTML", "JQuery", "CSS", "XML"], 
+//         "CSS"
+//     ),
+//     new Question(
+//         "There are ____ main components of object oriented programming.", 
+//         ["1", "6","2", "4"], 
+//         "4"
+//     ),
+//     new Question(
+//         "Which language is used for web apps?", 
+//         ["PHP", "Python", "Javascript", "All"], 
+//         "All"
+//     ),
+//     new Question(
+//         "MVC is a ____.", 
+//         ["Language", "Library", "Framework", "All"], 
+//         "Framework"
+//     )
+// ];
